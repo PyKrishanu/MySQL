2a3ef2de075bb0cac11a23688d88bdc789528108
PyKrishanu <krishanu.joarder@gmail.com>
2020-03-14 09:58:03 +0530

    del

--- views/sales_by_client.sql
+++
@@ -1,8 +0,0 @@
-create or replace view sales_by_client as
-select
-c.client_id,
-c.name,
-sum(invoice_total) as total_sales
-from clients c
-join invoices i using (client_id)
-group by client_id, name
